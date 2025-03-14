05d22426cb9d1240105b83e98ee940f5f48eae45
Dominik Kormann <alexander.kormann@outlook.de>
2025-03-14 12:39:12 +0100

    init

---
+++ examples/helloworld.py
@@ -0,0 +1,16 @@
+#%%
+from tinsor import Shape, Tensor, Dim
+
+
+S, T, U, V = Shape(S=5, T=3, U=4, V=6)
+
+x = Tensor.ones(S, T)
+y = Tensor.rand(T, U)
+
+p = x @ y
+
+print(p)
+
+p = x * y
+
+print(p)
---
+++ test/test_dim.py
@@ -0,0 +1,16 @@
+import unittest
+from tinsor import Dim
+
+class TestDim(unittest.TestCase):
+  def test_dim(self):
+    A = Dim('A', 3)
+
+    assert A.size == 3
+    assert A.name == 'A'
+
+    A2 = Dim('A', 3)
+
+    assert A2 == A
+
+    B = Dim('B', 3)
+    assert A != B
